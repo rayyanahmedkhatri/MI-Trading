51670abedbd651e9a7d98af2d57d97f40ca62786
rayyanahmedkhatri <rayyan.ahmed.khatri@gmail.com>
2025-01-17 12:14:51 +0500

    MM

--- products/hand-pallet-truck.php
+++ products/hand-pallet-truck.php
@@ -1,6 +1,6 @@
 <?php
 $productName = "Hand Pallet Truck";
-$productDescription = "";
+$productDescription = "Description : N/A";
 $productImage = "../assets/images/products/hand-pallet-truck.jpg";
 $productDrawing = "../assets/images/products/dimention-drawing/hand-pallet-truck.jpg";
 include('master.php');
